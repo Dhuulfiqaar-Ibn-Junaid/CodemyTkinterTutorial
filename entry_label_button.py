a36cc05249a5cade9cd44b72d6d743df2dfe5888
Dhuulfiqaar Ibn Junaid <96455339+Dhuulfiqaar-Ibn-Junaid@users.noreply.github.com>
2022-07-06 13:49:48 -0700

    Add files via upload

---
+++ entry_label_button.py
@@ -0,0 +1,31 @@
+# importing everything from the tkinter module
+from tkinter import *
+
+# Create a window for the application 
+root = Tk()
+
+# User Input Box and use e.get() to get the value
+e = Entry(root, bg="white", fg="black")
+e.pack()
+e.insert(0, "Username")
+
+# Define a function to be executed if my_btn clicked
+def myClick():
+    my_lbl = Label(root, text=e.get())
+    my_lbl.pack()
+
+# Create a button widget
+my_btn = Button(root, text="Click Me", padx=50, pady=50, command=myClick)
+"""
+1) padx and pady for the spacing inside the button 
+2) state=DISABLED to forbid clicking a button
+3) just add the command to the attribute of the button and it should work just like in javascript
+4) We don't need to add the parentheses after the function name in the command attribute of the function
+5) Change the text color of the button using fg (foreground) and the button using bg (background)
+"""
+
+# Just simply pack the button without worrying about the position on the window
+my_btn.pack()
+
+# Event loop constantly looping to listen for each loop (even the moving mouse)
+root.mainloop()
